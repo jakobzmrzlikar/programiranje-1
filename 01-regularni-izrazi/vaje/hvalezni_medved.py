c23de1903964f16fc536f9e53f4ae4c09a72c52d
Jakob <jakob.zmrzlikar@gmail.com>
2019-10-07 11:56:45 +0200

    solved first exercise

--- 01-regularni-izrazi/vaje/hvalezni_medved.py
+++ 01-regularni-izrazi/vaje/hvalezni_medved.py
@@ -1,3 +1,4 @@
+import re
 ###############################################################################
 # Hvaležni medved
 #
@@ -27,6 +28,14 @@ medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
 ###############################################################################
 
 
+def find_words(niz, podniz):
+    pattern = re.compile(rf'\b\w*{podniz}\w*\b')
+    return {match.group(0) for match in re.finditer(pattern, niz)}
+
+
+print(find_words(test_text, 'de'))
+
+
 ###############################################################################
 # 2) Sestavite funkcijo [find_prefix], ki vrne množico vseh besed, ki se
 #    pojavijo v nizu in imajo dano predpono.
@@ -34,6 +43,14 @@ medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
 # >>> find_prefix(test_text, 'zi')
 # {'zibala', 'zibel', 'zibelko'}
 ###############################################################################
+
+
+def find_prefix(niz, podniz):
+    pattern = re.compile(rf'\b{podniz}\w*\b')
+    return {match.group(0) for match in re.finditer(pattern, niz)}
+
+
+print(find_prefix(test_text, 'zi'))
 
 
 ###############################################################################
@@ -45,6 +62,14 @@ medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
 ###############################################################################
 
 
+def find_suffix(niz, podniz):
+    pattern = re.compile(rf'\b\w*{podniz}\b')
+    return {match.group(0) for match in re.finditer(pattern, niz)}
+
+
+print(find_suffix(test_text, 'la'))
+
+
 ###############################################################################
 # 4) Sestavite funkcijo [double_letters], ki sprejme niz in vrne množico vseh
 #    besed, ki vsebujejo podvojene črke.
@@ -52,3 +77,11 @@ medved. Zvrhano zibelko sladkih hrušk mi je prinesel za en sam izdrt trn"."""
 # >>> double_letters('A volunteer is worth twenty pressed men.')
 # {'volunteer', 'pressed'}
 ###############################################################################
+
+
+def double_letters(niz):
+    pattern = re.compile(rf'\b\w*(\w)\1\w*\b')
+    return {match.group(0) for match in re.finditer(pattern, niz)}
+
+
+print(double_letters('A volunteer is worth twenty pressed men.'))
